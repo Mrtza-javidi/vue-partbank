da8b517b5e6397906aca295ff605a08326c7aa41
morteza.javidi <morteza.javidi.webdev@gmail.comi>
2024-09-13 17:28:51 +0330

    feat: implement login-page and activate the router for login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const routes: any = [
+const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/login" },
   {
     path: "/login",
     name: "login",
     component: () => import("@/views/login-page.vue"),
   },
-  {
-    path: "/dashboard",
-    name: "dashboard",
-    component: () => import("@/views/dashboard-page.vue"),
-  },
-  {
-    path: "/personal-info",
-    name: "personal-info",
-    component: () => import("@/views/personal-info-page.vue"),
-  },
-  {
-    path: "/file-picker",
-    name: "file-picker",
-    component: () => import("@/views/file-picker-page.vue"),
-  },
-  {
-    path: "/confirm-info",
-    name: "confirm-info",
-    component: () => import("@/views/confirm-info-page.vue"),
-  },
-  {
-    path: "/:pathMatch(.*)*",
-    name: "not-found",
-    component: () => import("@/views/not-found-page.vue"), // Show a NotFound page first
-  },
+  // {
+  //   path: "/dashboard",
+  //   name: "dashboard",
+  //   component: () => import("@/views/dashboard-page.vue"),
+  // },
+  // {
+  //   path: "/personal-info",
+  //   name: "personal-info",
+  //   component: () => import("@/views/personal-info-page.vue"),
+  // },
+  // {
+  //   path: "/file-picker",
+  //   name: "file-picker",
+  //   component: () => import("@/views/file-picker-page.vue"),
+  // },
+  // {
+  //   path: "/confirm-info",
+  //   name: "confirm-info",
+  //   component: () => import("@/views/confirm-info-page.vue"),
+  // },
+  // {
+  //   path: "/:pathMatch(.*)*",
+  //   name: "not-found",
+  //   component: () => import("@/views/not-found-page.vue"), // Show a NotFound page first
+  // },
 ];
 
 const router = createRouter({
@@ -39,6 +39,6 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {});
+// router.beforeEach((to, from, next) => {});
 
 export default router;
