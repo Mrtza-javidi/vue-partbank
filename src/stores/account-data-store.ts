e65fd1b52b7f03fb6cfda98a3666c2dac6d0ac39
morteza.javidi <morteza.javidi.webdev@gmail.comi>
2024-09-14 21:13:10 +0330

    feat: implement bank account data store and remove extra comment from account-trasactions-store

---
+++ src/stores/account-data-store.ts
@@ -0,0 +1,82 @@
+import { defineStore } from "pinia";
+import axiosInstance from "@/services/axios/axios-config";
+import { enableMocking } from "@/services/axios/mock-axios";
+
+interface AccountData {
+  data: object;
+  isModalVisible: boolean;
+  isLoading: boolean;
+  mockEnabled: boolean;
+}
+
+export const useAccountDataStore = defineStore("account-data", {
+  state: (): AccountData => ({
+    data: {},
+    isModalVisible: true,
+    isLoading: false,
+    mockEnabled: true,
+  }),
+  actions: {
+    async fetchAccountData() {
+      this.isLoading = true;
+      try {
+        if (this.mockEnabled) {
+          const mockResponse = {
+            data: {
+              status: "success",
+              result: {
+                firstName: "مرتضی",
+                lastName: "جاویدی",
+                address: "همین دور و بر",
+                postalCode: "1212121212",
+                balance: 92000000,
+                cardNumber: "1395516919687507",
+                score: {
+                  amount: 20000000,
+                  paymentPeriod: 48,
+                },
+                upcomingInstalment: {
+                  amount: 1000000,
+                  dueDate: "20250307",
+                },
+                id: "30ab1b8c-4a14-40fc-bd0a-4927c85263ee",
+              },
+            },
+          };
+          this.data = mockResponse.data;
+        } else {
+          const response = await axiosInstance.get("/deposit-account");
+          if (
+            response &&
+            response.result &&
+            Object.keys(response.result).length !== 0
+          ) {
+            this.data = response.result;
+            this.isModalVisible = false;
+          } else {
+            this.isLoading = false;
+            this.isModalVisible = true;
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching account data:", error);
+        this.isModalVisible = true;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    enableMocking(state: boolean): void {
+      this.mockEnabled = state;
+      enableMocking(state);
+    },
+  },
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        key: "account-data",
+        storage: localStorage,
+      },
+    ],
+  },
+});
